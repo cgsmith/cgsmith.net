be8c3862538af85ca2b8c5e87edcf4af2b7b5691
Chris Smith <chris@cgsmith.net>
2015-09-01 22:03:11 -0500

    Trying to get before install shell to work

--- scripts/installhugo.sh
+++ scripts/installhugo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_linux_amd64.tar.gz ~/
-tar -xvzf ~/hugo_0.14_linux_amd64.tar.gz
-mv ~/hugo_0.14_linux_amd64/hugo_0.14_linux_amd64 /usr/local/bin/hugo
+wget https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_linux_amd64.tar.gz /home/ec2-user
+tar -xvzf /home/ec2-user/hugo_0.14_linux_amd64.tar.gz
+mv /home/ec2-user/hugo_0.14_linux_amd64/hugo_0.14_linux_amd64 /usr/local/bin/hugo
