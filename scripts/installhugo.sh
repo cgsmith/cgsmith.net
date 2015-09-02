1b24b0270d2ab4b5d8dae66e5c39be3b04105d41
Chris Smith <chris@cgsmith.net>
2015-09-01 22:19:16 -0500

    Have the script CD and then do operations

--- scripts/installhugo.sh
+++ scripts/installhugo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_linux_amd64.tar.gz -O /home/ec2-user/hugo_0.14_linux_amd64.tar.gz
-tar -xvzf /home/ec2-user/hugo_0.14_linux_amd64.tar.gz /home/ec2-user/
-mv /home/ec2-user/hugo_0.14_linux_amd64/hugo_0.14_linux_amd64 /usr/local/bin/hugo
+cd /home/ec2-user
+wget https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_linux_amd64.tar.gz
+tar -xvzf hugo_0.14_linux_amd64.tar.gz
+mv hugo_0.14_linux_amd64/hugo_0.14_linux_amd64 /usr/local/bin/hugo
