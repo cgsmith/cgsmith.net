746f31b7e9daa829c0181b9559a180fa6a1cd7a9
Chris Smith <chris@cgsmith.net>
2015-09-01 22:26:15 -0500

    Hugo to /usr/bin/hugo

--- scripts/installhugo.sh
+++ scripts/installhugo.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user
 wget https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_linux_amd64.tar.gz
 tar -xvzf hugo_0.14_linux_amd64.tar.gz
-mv hugo_0.14_linux_amd64/hugo_0.14_linux_amd64 /usr/local/bin/hugo
+mv hugo_0.14_linux_amd64/hugo_0.14_linux_amd64 /usr/bin/hugo
